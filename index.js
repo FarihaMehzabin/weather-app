70b20eb9bd3dd779e927d607f2272b8979b92c08
Fariha Mehzabin <henlothere45@gmail.com>
2022-12-09 22:10:46 +0600

    fix date

--- index.js
+++ index.js
@@ -4,15 +4,46 @@ const temp = document.getElementById("temp__txt");
 const humidity = document.getElementById("humidity__txt");
 const feels_like = document.getElementById("feels-like");
 const forecast_div = document.getElementById("forecast");
+const date_time = document.getElementById("date-time");
+const month_array = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const day_array = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const date = new Date();
+const day = date.getDate();
+const month = month_array[date.getMonth()];
+const weekDay = day_array[date.getDay()];
 
 forecast_div.style.display = "none";
 
+console.log(day, month, weekDay)
 
 
 // console.log(place.value)
 
 place.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
+    date_time.textContent = `${weekDay}, ${day} ${month}`
     forecast_div.classList.remove("forecast"); // reset animation
     void forecast_div.offsetWidth; // trigger reflow
     forecast_div.classList.add("forecast"); // start animation
