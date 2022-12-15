829e438a90fed297e898c60ecf657f41c2cfe382
Fariha Mehzabin <henlothere45@gmail.com>
2022-12-15 17:32:35 +0600

    add lock

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import json
 import time
 import requests
+import threading
 from cache import CacheByMe
 from weather_data import WeatherData
 from rate_limiter_for_Ip import Limiter
@@ -29,28 +30,39 @@ def index():
     try:
         ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
         
+        lock = threading.Lock()
         
-        if(limiter.check_if_limited(ip_addr)):
-            limiter_data = jsonify(rate_limit_response="rate limit reached. Please try again in 10 seconds.")
-            limiter_data.headers.add('Access-Control-Allow-Origin', '*')
-            return limiter_data
+        # if(limiter.check_if_limited(ip_addr)):
+        #     limiter_data = jsonify(rate_limit_response="rate limit reached. Please try again in 10 seconds.")
+        #     limiter_data.headers.add('Access-Control-Allow-Origin', '*')
+        #     return limiter_data
         
         
         source = request.args.get(
             "city"
         )  # getting parameters from url. Whatever comes after ? is a parameter
         
+        
+        lock.acquire()
+        print(source)
+        print(f'{time.time()} Off to fetch weather and locking {lock.locked()}')
+
+        
         # checking if exists in cache
         cache_check = cache_instance.check_in_cache(source.lower())
         if cache_check:
+            print(f"I'm from cache {lock.locked()}")
             cache_data = WeatherData.create_weather_json(cache_check)
             cache_data.headers.add('Access-Control-Allow-Origin', '*')
             return cache_data
+        
         
         # fetching weather using city name
         response = requests.get(
             f"http://api.openweathermap.org/data/2.5/weather?q={source}&units=metric&appid=106c8085ba2b900cce93846e18cedece"
         )
+        time.sleep(10)
+        print(f"getting weather for {source}")
         res = response.json()
         
         
@@ -69,11 +81,16 @@ def index():
         # saving in cache for 10mins
         cache_instance.add_weather_data(weather_data.city.lower(), weather_data)
         
+        lock.release()
+        print(f'{time.time()} Done fetching weather and unlocking {lock.locked()}')
+
+       
         print(cache_instance.data)
         # returning json to fetch
         data = WeatherData.create_weather_json(weather_data)
         data.headers.add('Access-Control-Allow-Origin', '*')
         return data
+    
 
     except Exception as err:
         print(err)
