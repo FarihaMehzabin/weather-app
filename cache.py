7155da4d1638e8bd6075a6f0447e167aac06cc39
Fariha Mehzabin <henlothere45@gmail.com>
2022-12-17 20:24:50 +0600

    add locks to cache

--- cache.py
+++ cache.py
@@ -9,51 +9,100 @@ class CacheByMe:
     
     def __init__(self):
         self.data = dict()
-        self.lock = threading.Lock()
-        
+        self.cities = dict()
+        self.lock = threading.Condition()
+    
+    def log(self, message):
+        print(f"[{datetime.now().strftime('%H:%M:%S')}] | Thread ID: {threading.get_ident()} {message}")    
         
     def get_weather_data(self, city):
         
-        # self.lock.acquire()
         cache_check = self.check_in_cache(city.lower())
         
+        self.log(f"Outside cache check start for {city}")
+        
         if cache_check:
-            print(f"[{datetime.now().strftime('%H:%M:%S')}]: I'm from cache")
+            self.log(f"Printed from cache for {city} ")
+            cache_data = WeatherData.create_weather_json(cache_check)
+            cache_data.headers.add('Access-Control-Allow-Origin', '*')
+            return cache_data  
+        else: 
+            self.log(f"🌞Locking begins in outside cache, trying to dump {city} in dict...")
+            self.lock.acquire()
+            self.log(f"nothing in cache for {city}")
+            if(city not in self.cities.keys()):
+                self.log(f'🎉added {city} to dict')
+                self.cities[city.lower()] = {'lock': threading.Lock()}
+            
+            self.lock.release()
+            self.log(f"Unlocked from outside cache")  
+         
+        self.log(f"Outside cache check ends for {city}")
+        
+        
+            
+        self.log(f"❌ Locking begins... for {city}")
+        
+        self.lock.acquire()
+        
+        cache_check = self.check_in_cache(city.lower())
+        
+        self.log(f"Inside cache check start for {city}")
+        
+        if cache_check:
+            self.log(f"Printed from cache for {city}")
             
             cache_data = WeatherData.create_weather_json(cache_check)
             cache_data.headers.add('Access-Control-Allow-Origin', '*')
-            return cache_data
+            self.lock.release()
+            self.log(f"🟢Unlocked from cache..for {city}")    
+            return cache_data  
+        else: 
+            self.log(f"nothing in inside cache for {city}")
         
-        # self.lock.release()
+        self.log(f"Inside cache check ends for {city}")
+        
+        
+        if city.lower() in self.cities.keys(): 
+            self.log(f'locked and trying to fetch weather for {city}')
+            self.cities[city.lower()]['lock'].acquire()
+        self.log(f'🙌 Done fetching weather for {city} ')
         response = requests.get(
-            f"http://api.openweathermap.org/data/2.5/weather?q={city.lower()}&units=metric&appid=106c8085ba2b900cce93846e18cedece"
-        )
+                        f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=106c8085ba2b900cce93846e18cedece"
+                    )
+       
         
-        
-        print(f"[{datetime.now().strftime('%H:%M:%S')}]: Looking for {city} weather.")
-        
-        
+            
+        self.log("Start sleep for 10s")
+        time.sleep(10)
+        self.log("End sleep for 10s")
+            
         res = response.json()
         
         weather_data = WeatherData(
-            res["name"],
-            res["weather"][0]["description"],
-            str(time.time()),
-            str(time.time()+(60*10)),
-            res["main"]["temp"],
-            res["main"]["feels_like"],
-            res["main"]["humidity"],
-        )
-        
+                res["name"],
+                res["weather"][0]["description"],
+                str(time.time()),
+                str(time.time()+(60*10)),
+                res["main"]["temp"],
+                res["main"]["feels_like"],
+                res["main"]["humidity"],
+            )
+            
+        self.log(f"Adding to cache..for {city}")
         self.add_weather_data(weather_data.city.lower(), weather_data)
+        
+        self.cities[city.lower()]['lock'].release()
+        self.log(f'Unlocking & Finished adding {city} weather to cache')
+        
+        
+        self.lock.release()
+        self.log(f"Unlocking...")
         
         data = WeatherData.create_weather_json(weather_data)
         data.headers.add('Access-Control-Allow-Origin', '*')
-        
-        time.sleep(10)
-        print(f"[{datetime.now().strftime('%H:%M:%S')}]: slept for 10s")
-        
-        return data
+           
+        return data        
     
     
     def check_in_cache(self, city):
@@ -64,12 +113,16 @@ class CacheByMe:
         time_passed = time.time() - self.data[city]['time_set']
         if time_passed>=600:
             del cache_data[city.lower()]
+            # del self.lock_dict[city.lower()]
             return False   
         else:
             return self.data[city]['value']
         
     def add_weather_data(self,city, weather_data):
         self.data[city.lower()] = {'value': weather_data, 'time_set': time.time()}
+        
+    
+    
         
    
             
---
+++ req_script.py
@@ -0,0 +1,36 @@
+# from threading import Thread
+# import time
+# import requests
+
+
+# arr = ['london', 'london', 'New York', 'New York']
+
+# def run_request(src):
+#     requests.get(
+#             f"http://127.0.0.1:8080?city={src}"
+#         )
+
+# for x in range(4):
+#     t = Thread(target=run_request, args=('london'))
+#     t.daemon = True
+#     t.start()
+
+# time.sleep(10)
+
+
+import concurrent.futures
+import time
+import requests
+
+arr = ['london', 'london', 'New York', 'New York']
+THREAD_POOL = 5
+
+
+def run_request(src):
+    requests.get(
+            f"http://127.0.0.1:8080?city={src}"
+        )
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=THREAD_POOL) as executor:
+    executor.map(run_request, arr)
